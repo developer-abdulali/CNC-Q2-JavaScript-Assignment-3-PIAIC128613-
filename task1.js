d7214bb9fabc0b607ba1c41115819d88086cb5c1
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-03-08 22:59:46 +0500

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,11 @@
+var qualifications = ['1) SSC','2) HSC','3) BCS','4) BS','5) BCOM','6) MS','7) M. Phil','8) PhD'];
+
+document.write ('Qualifications:' + '<br>');
+document.write(qualifications[0] + '<br>');
+document.write(qualifications[1] + '<br>');
+document.write(qualifications[2] + '<br>');
+document.write(qualifications[3] + '<br>');
+document.write(qualifications[4] + '<br>');
+document.write(qualifications[5] + '<br>');
+document.write(qualifications[6] + '<br>');
+document.write(qualifications[7] + '<br>');
